78f4be0bc328c03e1f543bb3dbccddb9ddc7202b
Mini-Sylar <75648051+Mini-Sylar@users.noreply.github.com>
2022-02-07 12:43:41 +0000

    Added Screenshots

--- README.md
+++ README.md
@@ -4,5 +4,11 @@
 - Fully Responsive :)
 - Hamburger Menu
 
+## Screenshots
+
+![Laptop](./Screenshots/laptop.png)
+![Laptop](./Screenshots/tablet.png)
+![Laptop](./Screenshots/mobile.png)
+
 ## Credit 
 [Md. Maksedur rahman](https://dribbble.com/shots/17335386-Cryptocurrncy-Header-Exploration)
--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 let ham = document.querySelector(".hamburger");
 let navbar = document.querySelector(".navbar");
 let links  = document.querySelector('.links')
+let login  = document.querySelector('.loginbutton')
 
 ham.addEventListener("click", function () {
   navbar.classList.toggle("showlinks");
   if (navbar.classList.contains("showlinks")) {
    links.classList.remove('hidelinks')
+   login.classList.remove("hidelinks");
   }
   else{
      links.classList.add("hidelinks");
+      login.classList.add("hidelinks");
   }
 });
